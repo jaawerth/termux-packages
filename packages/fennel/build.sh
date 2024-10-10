3cd49095909fdf17204f33a624e7d448f20a10ab
jaawerth <jaawerth@gmail.com>
2024-10-10 17:39:46 -0400

    allow any of lua5.1-5.4 or luajit

--- packages/fennel/build.sh
+++ packages/fennel/build.sh
@@ -5,11 +5,33 @@ TERMUX_PKG_MAINTAINER="@termux"
 TERMUX_PKG_VERSION=1.5.1
 TERMUX_PKG_SRCURL="https://github.com/bakpakin/Fennel/archive/refs/tags/$TERMUX_PKG_VERSION.tar.gz"
 TERMUX_PKG_SHA256=7456737a2e0fc17717ea2d80083cfcf04524abaa69b1eb79bded86b257398cd0
-TERMUX_PKG_DEPENDS="lua53"
+TERMUX_PKG_DEPENDS="lua54|lua53|lua52|luajit|lua51"
+TERMUX_PKG_BUILD_DEPENDS="lua54|lua53|lua52|luajit|lua51"
 TERMUX_PKG_PLATFORM_INDEPENDENT=true
 TERMUX_PKG_BUILD_IN_SRC=true
 
+iscmd() {
+	command -v "$1" >/dev/null
+}
+
 termux_step_pre_configure() {
-	export LUA_VERSION=5.3
-	export LUA=lua5.3
+	local luabin luadeps luadep
+	IFS=\| read -r -a luadeps <<< "$TERMUX_PKG_BUILD_DEPENDS"
+	for luadep in "${luadeps[@]}"; do
+		luabin=$(sed 's/lua5([1-4])/lua5.\1/' <<< "$luadep")
+		case "$luadep" in
+			lua5*)
+				luabin="lua5.${luadep#lua5}"
+				;;
+			*)
+				luabin="$luadep"
+				;;
+		esac
+		if command -v "$LUA" >/dev/null; then
+			export LUA="$luabin"; break
+		else
+			continue
+		fi
+
+	done
 }
